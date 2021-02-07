05e20ca1062d4b4ff66d20d2809a83f9a1ea90af
wilkison martins <wilkisonmartinsdasilva@gmail.com>
2021-02-07 15:32:09 -0400

    primeiro commit. Adicionado a barra inicial com saldo e os 3 primeiros cardviews fixos

---
+++ app/src/main/java/com/example/bancointer/ui/activities/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.bancointer.ui.activities;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bancointer.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
