a370f78680710e022285e1cec87b64483f84cb0a
wilkison martins <wilkisonmartinsdasilva@gmail.com>
2021-02-08 16:48:56 -0400

    ajustado o icone para esconder o saldo.

--- app/src/main/java/com/example/bancointer/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/bancointer/ui/activities/MainActivity.java
@@ -11,6 +11,7 @@ import android.transition.TransitionManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.bancointer.R;
 
@@ -19,7 +20,9 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageViewOcultador;
     private ConstraintLayout constraintLayout_segunda_linha;
     private ConstraintLayout constraintLayout_ocultador;
-
+    private ImageView imageViewEye;
+    private boolean mostrarSaldo = true;
+    private TextView textViewSaldo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,5 +56,27 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        imageViewEye = findViewById(R.id.imageViewEye);
+        textViewSaldo = findViewById(R.id.textview_saldo);
+        imageViewEye.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+                if (mostrarSaldo){
+                    imageViewEye.setImageResource(R.drawable.ic_eye_blocked);
+                    textViewSaldo.setText("_ _ _");
+                    mostrarSaldo = false;
+                }
+                else {
+                    imageViewEye.setImageResource(R.drawable.ic_eye_open);
+                    textViewSaldo.setText("2.916,18");
+                    mostrarSaldo = true;
+                }
+
+
+            }
+        });
+
     }
 }
