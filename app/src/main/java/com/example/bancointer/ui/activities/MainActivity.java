30453791b9f9c1f68f475c08a873a2c4324ab842
wilkison martins <wilkisonmartinsdasilva@gmail.com>
2021-02-08 11:01:37 -0400

    trocado o icone de esconder os menus e adicionado efeito de transição da seta

--- app/src/main/java/com/example/bancointer/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/bancointer/ui/activities/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.bancointer.ui.activities;
 
 import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Build;
@@ -12,22 +10,15 @@ import android.transition.AutoTransition;
 import android.transition.TransitionManager;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.example.bancointer.R;
 
-import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
-import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
-
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imageViewOcultador;
-    private ConstraintLayout constraintLayout;
-    CardView cardView4;
-    CardView cardView5;
-    CardView cardView6;
-    boolean mostrando = true;
+    private ConstraintLayout constraintLayout_segunda_linha;
+    private ConstraintLayout constraintLayout_ocultador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +26,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageViewOcultador = findViewById(R.id.imageview_ocultador);
-        cardView4 = findViewById(R.id.cardview_calendario4);
-        cardView5 = findViewById(R.id.cardview_calendario5);
-        cardView6 = findViewById(R.id.cardview_calendario6);
-        constraintLayout = findViewById(R.id.constraint_layout_segunda_linha);
+        constraintLayout_segunda_linha = findViewById(R.id.constraint_layout_segunda_linha);
+        constraintLayout_ocultador = findViewById(R.id.constraint_layout_ocultador);
 
         imageViewOcultador.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.KITKAT)
@@ -46,17 +35,21 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Log.i("ImageView", "Clicado");
 
-                if (mostrando) {
-                    TransitionManager.beginDelayedTransition(constraintLayout,
+                if (constraintLayout_segunda_linha.getVisibility() == View.VISIBLE) {
+                    TransitionManager.beginDelayedTransition(constraintLayout_segunda_linha,
                             new AutoTransition());
-                    constraintLayout.setVisibility(View.GONE);
-                    mostrando = false;
+                    constraintLayout_segunda_linha.setVisibility(View.GONE);
+                    TransitionManager.beginDelayedTransition(constraintLayout_ocultador,
+                            new AutoTransition());
+                    imageViewOcultador.setImageResource(R.drawable.ic_mostrar_mais);
                 }
                 else {
-                    TransitionManager.beginDelayedTransition(constraintLayout,
+                    TransitionManager.beginDelayedTransition(constraintLayout_segunda_linha,
                             new AutoTransition());
-                    constraintLayout.setVisibility(View.VISIBLE);
-                    mostrando = true;
+                    constraintLayout_segunda_linha.setVisibility(View.VISIBLE);
+                    TransitionManager.beginDelayedTransition(constraintLayout_ocultador,
+                            new AutoTransition());
+                    imageViewOcultador.setImageResource(R.drawable.ic_mostrar_menos);
                 }
             }
         });
